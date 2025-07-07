f31d27bc7a4189ce644d2c18892b44a6402226c5
toma-kazuki <kaz6032kaz@g.ecc.u-tokyo.ac.jp>
2025-07-07 11:42:03 -0500

    replace with dummy instruction/webdev config to local setting

--- daphne-at-interface/webpack.dev.js
+++ daphne-at-interface/webpack.dev.js
@@ -8,10 +8,10 @@ module.exports = merge(common, {
     mode: 'development',
     plugins: [
         new webpack.DefinePlugin({
-            // 'API_URL': JSON.stringify('http://localhost:8081/api/'),
-            // 'WS_URL': JSON.stringify('ws://localhost:8081/api/')
-            'API_URL': JSON.stringify('http://localhost/api/'),
-            'WS_URL': JSON.stringify('ws://localhost/api/')
+            'API_URL': JSON.stringify('http://localhost:8081/api/'),
+            'WS_URL': JSON.stringify('ws://localhost:8081/api/')
+            // 'API_URL': JSON.stringify('http://localhost/api/'),
+            // 'WS_URL': JSON.stringify('ws://localhost/api/')
         })
     ],
     devtool: 'eval-source-map',
